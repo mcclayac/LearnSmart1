794f02844fc6285d7c4c9a1523d8f5355610f2cb
Anthony McClay <anthony.mcclay@nielsen.com>
2016-07-12 19:54:30 -0400

    Initial Check-in

---
+++ IfStatements.py
@@ -0,0 +1,32 @@
+__author__ = 'anthonymcclay'
+__project__ = 'LearnSmart1'
+__date__ = '7/12/16'
+__revision__ = '$'
+__revision_date__ = '$'
+
+
+var = 100
+if var:
+    print("A true expression value")
+    print(var)
+
+var = 0
+if var:
+    print("A true expression value")
+    print(var)
+else:
+    print("Goodbye")
+
+# while
+
+count = 0
+while (count < 5):
+    print(' The Count is:', count)
+    count = count + 1
+print("Good Bye!")
+
+for var in range(5):
+    print("for loop : ", var)
+
+
+
