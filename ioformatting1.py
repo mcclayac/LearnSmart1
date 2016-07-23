329b16d11822683b95d8f9f1822a2b2a915c2797
Anthony McClay <anthony.mcclay@nielsen.com>
2016-07-22 23:51:58 -0400

    Initial Check-in 2

--- ioformatting1.py
+++ ioformatting1.py
@@ -6,6 +6,35 @@ __revision_date__ = '$'
 
 
 
-print("\nIO Formatting 1\n\n")
-import Class7IO.formattingExample1
+print("\nIO Formatting 1\n")
+from Class7IO.formattingExample1 import example1, example2, example3, example4
+
+
+example1()
+
+example2()
+
+example3()
+
+example4()
+
+
+from Class7IO.formattingExample2 import format1, format2, format3, format4
+
+print("\n\nIO Formatting 2\n")
+
+format1()
+
+format2()
+
+format3()
+
+format4()
+
+
+
+
+
+
+
 
