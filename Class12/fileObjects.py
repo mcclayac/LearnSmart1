423e7027ff732a029bd8eb7dfb02199d57eb8249
Anthony McClay <anthony.mcclay@nielsen.com>
2016-11-03 16:44:06 -0400

    Mysql testdb

--- Class12/fileObjects.py
+++ Class12/fileObjects.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-
+from stat import *
 
 def fileObjectsWrite():
 
@@ -83,4 +83,190 @@ def fileAccessibility():
     os.chmod('example.txt', stat.S_IWOTH )
 
     #
+
+
+def pathlib():
+
+    print("Path Lib Functions")
+
+    # Importing pathlib Library
+    from pathlib import Path
+
+    #Listing Subdirectories
+    pathVariable = Path(".")
+
+    [item for item in pathVariable.iterdir() if item.is_dir()]
+    print("display path of directories : \n", [item for item in pathVariable.iterdir() if item.is_dir()] , "\n")
+
+    for item in pathVariable.iterdir():
+        if item.is_dir():
+            posixPath = item
+            name = posixPath.name
+            print("dir: ", item, " ", name)
+
+    # listing Python source files in this directory tree
+    list(pathVariable.glob('*.py'))
+    print("\nDisplay path of the files : ", list(pathVariable.glob('*.py')))
+
+    # navigating inside a directory tree
+    # pathVariable = Path('c:/python34/demo')
+    # fileVariable = pathVariable / 'Module12' / '..'
+    # print("\nDisplay path of the current file :", fileVariable)
+
+
+    # listing Python source files in this directory tree
+    # list(pathVariable.glob('*.py'))
+    # print("\nDisplay path of the files : ", list(pathVariable.glob('*.py')))
+
+    # querying path properties
+    #print("\nWhether the path points to an existing file or directory : ", fileVariable.exists())
+
+    # opening a file
+    # with fileVariable.open() as file:
+    #     line = file.readline()
+    #     print("File content : ", line)
+    #
+    # file.close()
+
+
+
+
+def dirtree(top, callback):
+    """Recursively decend the directory tree rooted at the top
+        calling the callback function for each regular file
+    """
+    # Imports
+    import os, sys, stat
+
+
+    for f in os.listdir(top):
+        pathName = os.path(top, f)
+        mode = os.stat(pathName).st_mode
+
+        if S_ISDIR(mode):
+            # It's a directory, recurse into it
+            dirtree(pathName, callback)
+        elif S_ISREG(mode):
+            callback(pathName)
+        else:
+            print("Skipping %s" % pathName)
+
+def visitFile(file):
+    print("visiting", file)
+
+
+
+
+def tempFile():
+    print("\nExample program:\n")
+
+    # to import tempfile module
+    import tempfile
+
+    # create a temporary file and write some data to it
+    fp = tempfile.TemporaryFile()
+    fp.write(b'Hello world!')
+
+    # read data from file
+    fp.seek(0)
+    fp.read()
+
+    # close the file, it will be removed
+    fp.close()
+
+    # create a temporary file using a context manager
+    with tempfile.TemporaryFile() as fp:
+        fp.write(b'Hello world!')
+        fp.seek(0)
+        fp.read()
+
+    # file is now closed and removed
+    # create a temporary directory using the context manager
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        print('created temporary directory: ', tmpdirname)
+
+    # get temporary directory
+    print("get temporary directory: ", tempfile.gettempdir())
+
+
+def lzmaCompression():
+
+
+    print("\nExample program:\n")
+
+    # creating a compressed file
+    import lzma
+    data = b"Wellcome to Python"
+    with lzma.open("example.xz", "w") as file:
+        file.write(data)
+    file.close()
+
+    # reading in a compressed file
+    with lzma.open("example.xz") as file:
+        filecontent = file.read()
+        print("\nContent in the file : ", filecontent)
+    file.close()
+
+    # compressing data in memory
+    data_in = b"Wellcome to Python"
+    data_out = lzma.compress(data_in)
+    print("\nbefore Compress : ", data_in)
+    print("\nafter Compress : ", data_out)
+
+    # incremental compression
+    lzcompression = lzma.LZMACompressor()
+    out1 = lzcompression.compress(b"Some data\n")
+    out2 = lzcompression.compress(b"Another piece of data\n")
+    out3 = lzcompression.compress(b"Even more data\n")
+    out4 = lzcompression.flush()
+
+    # concatenate all the partial results:
+    result = b"".join([out1, out2, out3, out4])
+    print("\nresults:", result)
+
+    data = b"This data will be compressed\n"
+
+    # writing compressed data to an already-open file
+    with open("example.xz", "wb") as file:
+        with lzma.open(file, "w") as lzfile:
+            lzfile.write(data)
+
+
+
+
+def gzipCOmpression():
+    # !/usr/bin/python
+
+    print("\nExample program:\n")
+
+    # to create a compressed GZIP file
+    import gzip
+    data = b"Wellcome to Python Programming"
+    with gzip.open('example.txt.gz', 'wb') as file:
+        file.write(data)
+
+    file.close()
+
+    # to read a compressed file
+    with gzip.open('example.txt.gz', 'rb') as file:
+        filecontent = file.read()
+        print("File content : ", filecontent)
+    file.close()
+
+    # to GZIP compress an existing file
+    with open('example.txt', 'rb') as filein:
+        with gzip.open('example.txt.gz', 'wb') as fileout:
+            fileout.writelines(filein)
+
+    filein.close()
+    fileout.close()
+
+    # to GZIP compress a binary string
+    inputString = b"Lots of content here"
+    outputString = gzip.compress(inputString)
+    print(" Input: ", inputString)
+    print(" Output: ", outputString)
+
+
+
 
--- ch12_fileObjects.py
+++ ch12_fileObjects.py
@@ -2,12 +2,29 @@
 
 
 from Class12.fileObjects import fileObjectsWrite, \
-    fileObjectsRead, fileAccessibility
+    fileObjectsRead, fileAccessibility, pathlib, dirtree, visitFile, \
+    tempFile, lzmaCompression, gzipCOmpression
 
 
+
+
+
+import sys
 
 #fileObjectsWrite()
 
 #fileObjectsRead()
+#fileAccessibility()
 
-fileAccessibility()
+#pathlib()
+
+# execute from command prompt with directory name as an argument.
+# if __name__ == '__main__':
+#     dirtree(sys.argv[1], visitFile)
+
+#tempFile()
+
+#lzmaCompression()
+
+
+gzipCOmpression()
